6fe8af027738f27afa958be8c80deb8b038e5a6a
Hawra-14 <hawraaabdulla535@gmail.com>
2026-06-23 20:23:03 +0300

    reset button added with the wins' possibilities

--- app.js
+++ app.js
@@ -2,35 +2,38 @@
 const msg = document.querySelector('#message')
 const brd = document.querySelector('.board')
 const sqr = document.querySelectorAll('.sqr')
+const reset = document.querySelector('button')
 
 let playerX = true
 let playerO = false
-let winner = ''
+// let winner = ''
 
 brd.addEventListener('click', function (event) {
-    console.log(event.target)
-    console.log(sqr[0].textContent)
+    // console.log(event.target)
+    // console.log(sqr[0].textContent)
+
+    if (!event.target.classList.contains('sqr')) { // applied to sqr class only 
+        return
+    }
 
     if (playerX === true) {
         // type X and disable the sqr
         let sqrX = sqr[event.target.id]
         sqrX.textContent = 'X'
-        // console.log(sqrID)
+        // console.log(sqrX)
         sqrX.classList.add('disable')
 
         playerX = false
         playerO = true
 
         msg.textContent = "It's O's turn"
-
-
     }
 
     else {
         // type O and disable the sqr
         let sqrO = sqr[event.target.id]
         sqrO.textContent = 'O'
-        // console.log(sqrID)
+        // console.log(sqrO)
         sqrO.classList.add('disable')
 
         playerX = true
@@ -41,22 +44,65 @@ brd.addEventListener('click', function (event) {
 
     // console.log(sqr[0].textContent)
 
-    if (sqr[0].textContent === sqr[1].textContent === sqr[2].textContent) {
-        console.log('inside the if statement')
+    if ((sqr[0].textContent !== '') && (sqr[0].textContent === sqr[1].textContent) && (sqr[1].textContent === sqr[2].textContent)) {
+        // console.log('inside the if statement')
         msg.textContent = `${sqr[0].textContent} wins!`
         brd.classList.add('disable')
-    }
-    if (sqr[0].textContent === 'X' && sqr[3].textContent === 'X' && sqr[6].textContent === 'X') {
-        msg.textContent = 'X wins!'
+    } else if ((sqr[3].textContent !== '') && (sqr[3].textContent === sqr[4].textContent) && (sqr[4].textContent === sqr[5].textContent)) {
+        msg.textContent = `${sqr[3].textContent} wins!`
         brd.classList.add('disable')
+    } else if ((sqr[6].textContent !== '') && (sqr[6].textContent === sqr[7].textContent) && (sqr[7].textContent === sqr[8].textContent)) {
+        msg.textContent = `${sqr[6].textContent} wins!`
+        brd.classList.add('disable')
+    } else if ((sqr[0].textContent !== '') && (sqr[0].textContent === sqr[3].textContent) && (sqr[3].textContent === sqr[6].textContent)) {
+        msg.textContent = `${sqr[0].textContent} wins!`
+        brd.classList.add('disable')
+    } else if ((sqr[1].textContent !== '') && (sqr[1].textContent === sqr[4].textContent) && (sqr[4].textContent === sqr[7].textContent)) {
+        msg.textContent = `${sqr[1].textContent} wins!`
+        brd.classList.add('disable')
+    } else if ((sqr[2].textContent !== '') && (sqr[2].textContent === sqr[5].textContent) && (sqr[5].textContent === sqr[8].textContent)) {
+        msg.textContent = `${sqr[2].textContent} wins!`
+        brd.classList.add('disable')
+    } else if ((sqr[0].textContent !== '') && (sqr[0].textContent === sqr[4].textContent) && (sqr[4].textContent === sqr[8].textContent)) {
+        msg.textContent = `${sqr[0].textContent} wins!`
+        brd.classList.add('disable')
+    } else if ((sqr[2].textContent !== '') && (sqr[2].textContent === sqr[4].textContent) && (sqr[4].textContent === sqr[6].textContent)) {
+        msg.textContent = `${sqr[2].textContent} wins!`
+        brd.classList.add('disable')
+    } // After all win checks
+    else {
+        // Check if all squares are filled
+        let isFull = true
+        sqr.forEach(function(square){
+            if (square.textContent === '') {
+                isFull = false
+                return
+            }
+        })
+
+        // for (let i = 0; i < sqr.length; i++) {
+        //     if (sqr[i].textContent === '') {
+        //         isFull = false
+        //         return
+        //     }
+        // }
+
+        if (isFull) {
+            msg.textContent = 'It is a tie!'
+            brd.classList.add('disable')
+        }
+
     }
 })
 
-// check for winner function
-// decide if the game is over
-// then find the winner
-
-// winPossibilities() {
-//     const win = [0,1,2]
-// }
+reset.addEventListener('click', function () {
+    sqr.forEach(function (square) {
+        square.textContent = ''
+        // console.log(square.classList)
+        square.classList.remove('disable')
+    })
+    msg.textContent = "It's X's turn"
+    brd.classList.remove('disable')
+    playerX = true
+})
 
